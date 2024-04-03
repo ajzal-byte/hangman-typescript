a288a390fa14279ec6534a7391b6afcfb51416b4
ajzal-byte <ajzalaju10@gmail.com>
2024-04-03 13:46:41 +0530

    keyboard styles updated

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,20 @@
+import keys from "../data/keys.json";
+import styles from "../keyboard.module.css"
+
 const Keyboard = () => {
   return (
-    <div>Keyboard</div>
-  )
-}
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {keys.map((key) => (
+        <button className={`${styles.btn}`} key={key}>{key}</button>
+      ))}
+    </div>
+  );
+};
 
-export default Keyboard
+export default Keyboard;
