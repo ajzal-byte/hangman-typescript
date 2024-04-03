4b4bb0c37a041113a9a18a1239d01450b4191af9
ajzal-byte <ajzalaju10@gmail.com>
2024-04-03 10:38:08 +0530

    app layout builiding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-function App() {
-  return (
-    <h1>hi</h1>
-  )
-}
+import { useState } from "react";
+import words from "./data/wordList.json";
+import { HangmanDrawing, HangmanWord, Keyboard } from "./components";
 
-export default App
+const App = () => {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
+};
+
+export default App;
