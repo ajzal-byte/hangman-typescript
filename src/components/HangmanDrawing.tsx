55c6e64aecf6a45ee39ed47fa7df6a541595f717
ajzal-byte <ajzalaju10@gmail.com>
2024-04-03 11:42:32 +0530

    hangman style updated

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,7 +1,56 @@
+import {
+  HEAD,
+  BODY,
+  RIGHT_ARM,
+  LEFT_ARM,
+  RIGHT_LEG,
+  LEFT_LEG,
+} from "./BodyParts";
+
 const HangmanDrawing = () => {
   return (
-    <div>HangmanDrawing</div>
-  )
-}
+    <div style={{ position: "relative" }}>
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "250px",
+          background: "black",
+        }}
+      />
+    </div>
+  );
+};
 
-export default HangmanDrawing
+export default HangmanDrawing;
